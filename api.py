1c9c55e7f9b9d207939de679f22d1a7959e06051
Vitória Heliane <vitoriaheliane@gmail.com>
2023-02-02 18:07:25 -0300

    Update api.py

--- api.py
+++ api.py
@@ -6,7 +6,7 @@ import os
 app = Flask(__name__)
 api = Api(app)
 # cron = CronTab(tabfile='healthcheck-cron.tab')
-cron = CronTab(user='root')
+cron = CronTab(user='crontab')
 
 class SchedulerList(Resource):
   parser = reqparse.RequestParser()
